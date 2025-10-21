620ab6544d788f9a4b68f75bd44fe20e74b1aed8
QuintsGuy <knotttristan@gmail.com>
2025-10-21 16:53:30 -0400

    Created modal/dialog from Request an Appointment button

--- src/app/components/shared/contact-modal/contact-modal.component.ts
+++ src/app/components/shared/contact-modal/contact-modal.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-contact-modal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact-modal.component.html',
-  styleUrl: './contact-modal.component.scss'
+  styleUrls: ['./contact-modal.component.scss']
 })
 export class ContactModalComponent {
+  email: string = '';
+  
+  constructor(
+    public dialogRef: MatDialogRef<ContactModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { email?: string }
+  ) {
+    if (data && data.email) {
+      this.email = data.email;
+    }
+  }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  submit(): void {
+    this.dialogRef.close({ email: this.email });
+  }
 }
--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { NgIf } from '@angular/common';
+import { MatDialog } from '@angular/material/dialog';
+import { ContactModalComponent } from '../contact-modal/contact-modal.component';
 
 @Component({
   selector: 'app-header',
@@ -12,8 +14,20 @@ import { NgIf } from '@angular/common';
 export class HeaderComponent {
   menuOpen = false;
 
+  constructor(public dialog: MatDialog) {}
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
   }
 
+  openDialog(): void {
+    this.dialog.open(ContactModalComponent, {
+      panelClass: ['custom-dialog-container', 'no-padding-surface'],
+      backdropClass: 'custom-backdrop-class',
+      autoFocus: false,
+      restoreFocus: false,
+      width: '500px',
+      maxWidth: '90vw'
+    });
+  }
 }
